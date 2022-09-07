98af15fb003103b3177cb614e326621817cd6b38
marcio-grazianni <marcio.grazianni05@gmail.com>
2022-09-07 09:51:16 -0500

    PSR-12 testing

--- app/Http/Controllers/ListsController.php
+++ app/Http/Controllers/ListsController.php
@@ -7,13 +7,14 @@ use Illuminate\Http\Request;
 
 class ListsController extends Controller
 {
-
-
     private const SUV_VEHICLE = "000003";
     private const FIFITEEN_PERCENT_DISCOUNT = "85/100";
     private const THIRTY_PERCENT_DISCOUNT = "70/100";
+
     /**
      * Display a listing of the resource.
+     *
+     * @param Request $request
      *
      * @return \Illuminate\Http\Response
      */
@@ -23,35 +24,31 @@ class ListsController extends Controller
         $FilteredCategory = $request->input('category');
         $FilteredPrice = $request->input('price');
         $FilteredPriceEnabled = "off";
-        
-        $database = Lists::get();
+        $DatabaseProducts = Lists::get();
 
-        if($FilteredCategory !== NULL){
-        $database = Lists:: where('category', $FilteredCategory)->get();
-
-        }elseif($FilteredPrice !== NULL){
-            $database = Lists:: where('price', $FilteredPrice)->get();
+        if ($FilteredCategory !== 'NULL') {
+            $DatabaseProducts = Lists:: where('category', $FilteredCategory)->get();
+        } elseif ($FilteredPrice !== 'NULL') {
+            $DatabaseProducts = Lists:: where('price', $FilteredPrice)->get();
             $FilteredPriceEnabled = "on";
         }
-        
         $products = [];
-        foreach($database as $data)
-        {
+        foreach ($DatabaseProducts as $data) {
             $ItemCategory = $data["category"];
             $ItemSku = $data["sku"];
             $PriceOriginal = $PriceFinal = $data["price"];
-            $DiscountPercentage = NULL;
-            if($ItemCategory == "insurance"){
-                $PriceFinal = (int)($PriceOriginal)*self::THIRTY_PERCENT_DISCOUNT;
+            $DiscountPercentage = 'NULL';
+            if ($ItemCategory == "insurance") {
+                $PriceFinal = (int)($PriceOriginal) * self::THIRTY_PERCENT_DISCOUNT;
                 $DiscountPercentage = "30%";
             }
-            if($ItemSku == self::SUV_VEHICLE){
-                $PriceFinal = (int)($PriceOriginal)*self::FIFITEEN_PERCENT_DISCOUNT;
+            if ($ItemSku == self::SUV_VEHICLE) {
+                $PriceFinal = (int)($PriceOriginal) * self::FIFITEEN_PERCENT_DISCOUNT;
                 $DiscountPercentage = "15%";
             }
-            if($FilteredPriceEnabled == "on"){
+            if ($FilteredPriceEnabled == "on") {
                 $price = $PriceOriginal;
-            }else{
+            } else {
                 $price = collect(['original' => $PriceOriginal, 'final' => $PriceFinal,'discount_percentage' => $DiscountPercentage, 'currency' => "EUR"]);
             }
             $product = collect(['sku' => $data["sku"], 'name' => $data["name"],'category' => $data["category"], 'price' => $price]);
