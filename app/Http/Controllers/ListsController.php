3c36e235896b26849287b21d200c67d68d77ff23
marcio-grazianni <marcio.grazianni05@gmail.com>
2022-09-07 01:56:26 -0500

    tiny code

--- app/Http/Controllers/ListsController.php
+++ app/Http/Controllers/ListsController.php
@@ -7,134 +7,57 @@ use Illuminate\Http\Request;
 
 class ListsController extends Controller
 {
+
+
+    private const SUV_VEHICLE = "000003";
+    private const FIFITEEN_PERCENT_DISCOUNT = "85/100";
+    private const THIRTY_PERCENT_DISCOUNT = "70/100";
     /**
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
-        return Lists::get();
-    }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
+        $FilteredCategory = $request->input('category');
+        $FilteredPrice = $request->input('price');
+        $FilteredPriceEnabled = "off";
+        
+        $database = Lists::get();
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        $request->validate([
-            'description'=>'required',
-        ]);
+        if($FilteredCategory !== NULL){
+        $database = Lists:: where('category', $FilteredCategory)->get();
 
-        try{
-            Lists::create($request->post());
-
-            return response()->json([
-                'message'=>'Lists Created Successfully!!'
-            ]);
-        }catch(\Exception $e){
-            \Log::error($e->getMessage());
-            return response()->json([
-                'message'=>'Something goes wrong while creating a Lists!!'
-            ],500);
+        }elseif($FilteredPrice !== NULL){
+            $database = Lists:: where('price', $FilteredPrice)->get();
+            $FilteredPriceEnabled = "on";
         }
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Lists  $lists
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Lists $lists, $list)
-    {
-        $data = Lists::where('id', $list)->get();
-        return response()->json([
-            'list'=> $data
-        ]);
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Lists  $lists
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Lists $lists)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Lists  $lists
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Lists $lists, $list)
-    {
-        $request->validate([
-            'description'=>'required',
-            'active'=>'required',
-        ]);
-
-        try{
-
-            $Lists = Lists::find($list);
-            $Lists->description = $request->input('description');
-            $Lists->active = $request->input('active');
-            $Lists->update();
-
-
-            return response()->json([
-                'message'=>'Product Updated Successfully!!'
-            ]);
-
-        }catch(\Exception $e){
-            \Log::error($e->getMessage());
-            return response()->json([
-                'message'=>'Something goes wrong while updating a product!!'
-            ],500);
+        
+        $products = [];
+        foreach($database as $data)
+        {
+            $ItemCategory = $data["category"];
+            $ItemSku = $data["sku"];
+            $PriceOriginal = $PriceFinal = $data["price"];
+            $DiscountPercentage = NULL;
+            if($ItemCategory == "insurance"){
+                $PriceFinal = (int)($PriceOriginal)*self::THIRTY_PERCENT_DISCOUNT;
+                $DiscountPercentage = "30%";
+            }
+            if($ItemSku == self::SUV_VEHICLE){
+                $PriceFinal = (int)($PriceOriginal)*self::FIFITEEN_PERCENT_DISCOUNT;
+                $DiscountPercentage = "15%";
+            }
+            if($FilteredPriceEnabled == "on"){
+                $price = $PriceOriginal;
+            }else{
+                $price = collect(['original' => $PriceOriginal, 'final' => $PriceFinal,'discount_percentage' => $DiscountPercentage, 'currency' => "EUR"]);
+            }
+            $product = collect(['sku' => $data["sku"], 'name' => $data["name"],'category' => $data["category"], 'price' => $price]);
+            array_push($products, $product);
         }
-    }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Lists  $lists
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Lists $lists, $list)
-    {
-        try {
-
-           
-
-            Lists::where('id', $list)->delete();
-
-            return response()->json([
-                'message'=>'Product Deleted Successfully!!'
-            ]);
-            
-        } catch (\Exception $e) {
-            \Log::error($e->getMessage());
-            return response()->json([
-                'message'=>'Something goes wrong while deleting a product!!'
-            ]);
-        }
+        return $products;
     }
 }
